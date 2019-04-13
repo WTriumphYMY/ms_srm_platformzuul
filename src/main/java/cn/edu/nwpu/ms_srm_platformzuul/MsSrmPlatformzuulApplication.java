4be6f60ffde3dc83e8212088ee13a7298d4a89f4
WangYMY <triumph_wang@outlook.com>
2019-04-13 22:00:07 +0800

    zuul网关v0.0

---
+++ src/main/java/cn/edu/nwpu/ms_srm_platformzuul/MsSrmPlatformzuulApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.nwpu.ms_srm_platformzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class MsSrmPlatformzuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSrmPlatformzuulApplication.class, args);
+    }
+
+}
